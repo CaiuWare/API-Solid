c59c1871262cbd3c2eaecd3f67e788dc5f61c5b4
CaiuWare <caiu.ware@gmail.com>
2024-06-13 00:39:55 -0300

    PatternsAndTestes

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
-import { UsersRepositpry } from '../users-repository'
+import { UsersRepository } from '../users-repository'
 
-export class PrismaUsersRepository implements UsersRepositpry {
+export class PrismaUsersRepository implements UsersRepository {
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
